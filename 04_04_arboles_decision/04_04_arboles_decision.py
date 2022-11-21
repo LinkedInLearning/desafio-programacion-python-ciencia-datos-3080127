8ea6cd34cc0a8eddc7684e5efaff9153f457c383
Lincy <lincgr@gmail.com>
2022-11-21 16:15:51 -0600

    04_04_f Arboles de Decisión (Decision Tree Classifier)

---
+++ 04_04_arboles_decision/04_04_arboles_decision.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+
+X, y = make_moons(n_samples=5000, noise=0.3, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+arbol_decision = DecisionTreeClassifier()
+arbol_decision.fit(X_train, y_train)
+
+precision = arbol_decision.score(X_test, y_test)
+print('La presición es ', precision*100)
